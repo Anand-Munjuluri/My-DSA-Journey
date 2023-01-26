ba0f3be4c17ea6f1863d7617883a76291442169f
Anand Munjuluri <100225249+Anand-Munjuluri@users.noreply.github.com>
2023-01-26 19:04:40 +0530

    Add files via upload

---
+++ First-java/src/odd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class odd {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int n= input.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+        }
+    }
+
