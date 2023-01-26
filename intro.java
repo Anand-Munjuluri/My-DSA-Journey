9145ab8ef8aaa921cbe53d2409800a0c831fb671
Anand Munjuluri <100225249+Anand-Munjuluri@users.noreply.github.com>
2023-01-26 18:27:50 +0530

    Create intro.java

---
+++ intro.java
@@ -0,0 +1,15 @@
+
+  import java.util.Scanner;
+
+class intro {
+  public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+
+    String st=input.nextLine();
+    System.out.println(st);
+    System.out.print("bajji");
+    System.out.println("anand");
+    System.out.print("hjhdbwajh");
+    
+  }
+} 
